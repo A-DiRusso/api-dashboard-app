baf7387ee99b167d04d5fbff03611f83882505d5
Anthony DiRusso <ossurid@gmail.com>
2019-04-29 14:53:02 -0400

    adds weather.js and renders city, sky, temp, and a ron swanson quote

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import axios from 'axios';
+import Weather from './Weather';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h3>Weather</h3>
+        
+        <Weather />
       </header>
     </div>
   );
---
+++ src/Weather.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+
+export default class Weather extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        quote: '',
+        city: '',
+          sky: '',
+          temp: ''
+        
+      }
+  }
+  componentDidMount() {
+      this._getSwansonQuote();
+  }
+  render() {
+    return (
+      <div>
+        <h3>Weather And A Ron Swanson Quote</h3>
+        <div>{this.state.city}</div>
+        <div>{this.state.sky}</div>
+        <div>{this.state.temp}</div>
+        <div>{this.state.quote}</div>
+      </div>
+    )
+  }
+  _getSwansonQuote = async () => {
+      const zipcode = 30084;
+      const weatherApiKey = "03458bfd7dc0cbf327a517b3f034df4d";
+      const response = await axios.get(`http://api.openweathermap.org/data/2.5/forecast?zip=${zipcode},us&appid=${weatherApiKey}`)
+      const quoteResponse = await axios.get('https://ron-swanson-quotes.herokuapp.com/v2/quotes')
+      console.log(response.data.list[0].main.temp)
+      this.setState({
+          quote: quoteResponse.data,
+          city: response.data.city.name,
+          sky: response.data.list[0].weather[0].description,
+          temp: response.data.list[0].main.temp
+          
+    
+
+      })
+  }
+}
