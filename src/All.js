f70ae335a690cf64f4a097420c67abbf3089490b
Anthony DiRusso <ossurid@gmail.com>
2019-05-01 17:45:31 -0400

    adds All page for all the things

--- src/All.js
+++ src/All.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import './App.css';
+import Weather from './Weather';
+import Fortune from './Fortune';
+import Joke from './Joke';
+import Movie from './Movie'
+import GoT from './GoT';
+import Home from './Home';
 
-export default function All() {
-  return (
-    <div>
-      <h2>Everything in one place</h2>
+
+
+
+function All() {
+return (
+    <div className="App">
+    <header className="App-header">
+    <Weather />
+    <Fortune />
+    <Joke />
+    <Movie />
+    <GoT />
+    <Home />
+    </header>
     </div>
-  )
+);
 }
+
+export default All;
--- src/App.js
+++ src/App.js
@@ -15,31 +15,39 @@ import {
   Switch
 } from 'react-router-dom';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Link to="/home">Home</Link>
-        <Link to="/weather">Weather</Link>
-        <Link to="/fortune">Fortune</Link>
-        <Link to="/joke">Joke</Link>
-        <Link to="/movie">Movie</Link>
-        <Link to="/got">Got</Link>
-        <Link to="/all">All the Things</Link>
-
-        <Switch>
-          <Route path="/home" component={Home} />
-          <Route path="/weather" component={Weather} />
-          <Route path="/fortune" component={Fortune} />
-          <Route path="/joke" component={Joke} />
-          <Route path="/movie" component={Movie} />
-          <Route path="/got" component={GoT} />
-          <Route path="/all" component={All} />
-        </Switch>
-      </header>
-    </div>
-  );
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Link to="/home">Home</Link>
+          <Link to="/weather">Weather</Link>
+          <Link to="/fortune">Fortune</Link>
+          <Link to="/joke">Joke</Link>
+          <Link to="/movie">Movie</Link>
+          <Link to="/got">Got</Link>
+          <Link to="/all">All the Things</Link>
+  
+          <Switch>
+            <Route path="/home" component={Home} />
+            <Route path="/weather" component={Weather} />
+            <Route path="/fortune" component={Fortune} />
+            <Route path="/joke" component={Joke} />
+            <Route path="/movie" component={Movie} />
+            <Route path="/got" component={GoT} />
+            <Route path="/all" component={All} />
+          </Switch>
+        </header>
+      </div>
+    );
+  }
 }
+
 
 export default App;
